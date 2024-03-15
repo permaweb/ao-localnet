4b227831bcd83325648c94c3fbd7bd70e617102c
Michael Buhler <michael.buhler@arweave.org>
2024-03-15 20:58:44 +0700

    feat(seed): better scheduler-location publish script

--- seed/publish-scheduler-location.mjs
+++ seed/publish-scheduler-location.mjs
@@ -7,43 +7,27 @@ import { instance as arweave } from './utils/arweave.mjs'
 const wallet = JSON.parse(await readFile(import.meta.resolve('../wallets/scheduler-location-publisher-wallet.json').slice(7), 'utf8'))
 const address = await arweave.wallets.getAddress(wallet)
 
-console.log('address', address)
+console.log('scheduler location publisher address:', address)
 
-const tx = await arweave.createTransaction(
-  {
-    data: Math.random.toString().slice(-4),
-    tags: [
-      {
-        name: 'Data-Protocol',
-        value: 'ao',
-      },
-      {
-        name: 'Type',
-        value: 'Scheduler-Location',
-      },
-      {
-        name: 'Variant',
-        value: 'ao.LN.1',
-      },
-      {
-        name: 'URL',
-        value: 'http://localhost:4003',
-      },
-      {
-        name: 'Time-To-Live',
-        value: '86400',
-      },
-    ].map(({ name, value }) => ({
-      name: Buffer.from(name).toString('base64'),
-      value: Buffer.from(value).toString('base64'),
-    })),
-  },
-)
+const tx = await arweave.createTransaction({
+  data: Math.random.toString().slice(-4)
+})
+
+tx.addTag('Data-Protocol', 'ao')
+tx.addTag('Type', 'Scheduler-Location')
+tx.addTag('Variant', 'ao.LN.1')
+tx.addTag('Url', 'http://host.docker.internal:4003')
+tx.addTag('Time-To-Live', '1')
 
 await arweave.transactions.sign(tx, wallet)
 
-console.log(tx)
+console.log('POST /tx')
 
 const res = await arweave.transactions.post(tx)
 
-console.log(`${res.status} ${res.statusText}`)
+if (res.status === 200) {
+  console.log(`${res.status} ${res.statusText}`)
+  console.log(`tx id : ${tx.id}`)
+} else {
+  console.log(`${res.status} ${JSON.stringify(res.statusText, null, 2)}`)
+}
